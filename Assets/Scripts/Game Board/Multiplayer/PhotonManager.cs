a842f2d8f22fc9df1f5d2f12706ed106500d5041
koshaha <134874207+koshaha@users.noreply.github.com>
2023-07-04 18:12:54 +0300

    SyncLobyJoin

--- Assets/Scripts/Game Board/Multiplayer/PhotonManager.cs
+++ Assets/Scripts/Game Board/Multiplayer/PhotonManager.cs
@@ -13,7 +13,11 @@ public class PhotonManager : MonoBehaviourPunCallbacks
     [SerializeField] Room itemPrefab;
     [SerializeField] Transform content;
     private PhotonView photonView;
+    private bool readyCheck=false; 
     public GameObject roomContent;
+    public GameObject scrollView;
+
+
     
 
     void Update()
@@ -25,7 +29,6 @@ public class PhotonManager : MonoBehaviourPunCallbacks
     {
         PhotonNetwork.ConnectUsingSettings();
         photonView = GetComponent<PhotonView>();
-        roomContent.SetActive(true);
     }
 
     public override void OnConnectedToMaster()
@@ -40,7 +43,7 @@ public class PhotonManager : MonoBehaviourPunCallbacks
     {
         RoomOptions roomOptions = new RoomOptions();
         roomOptions.MaxPlayers =2;
-        PhotonNetwork.CreateRoom(roomName.text, roomOptions, TypedLobby.Default );
+        PhotonNetwork.CreateRoom(roomName.text+", ", roomOptions, TypedLobby.Default );
         
     }
     
@@ -52,27 +55,30 @@ public class PhotonManager : MonoBehaviourPunCallbacks
     public override void OnCreatedRoom()
     {
         Debug.Log("Created room with name: "+ PhotonNetwork.CurrentRoom.Name);
-        PhotonNetwork.LoadLevel("Multiplayer Game Board");
+        //PhotonNetwork.LoadLevel("Multiplayer Game Board");
     }
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
         Debug.LogError("Failed to create room");
     }
     
-    // public override void OnRoomListUpdate(List<RoomInfo> roomList)
-    // {
-    //     foreach (RoomInfo info in roomList)
-    //     {
-    //         Room listItem =Instantiate(itemPrefab, content);
-    //         if(listItem != null)
-    //             listItem.SetInfo(info);
-    //     }
-    // }
+     public override void OnRoomListUpdate(List<RoomInfo> roomList)
+     {
+         foreach (RoomInfo info in roomList)
+         {
+             Room listItem =Instantiate(itemPrefab, content);
+             if(listItem != null)
+                 listItem.SetInfo(info);
+         }
+     }
 
     public override void OnJoinedRoom()
     {
         Debug.Log ("You joined room: " + PhotonNetwork.CurrentRoom.Name );
-        PhotonNetwork.LoadLevel("Multiplayer Game Board");
+        scrollView.SetActive(false);
+        roomContent.SetActive(true);
+
+        //PhotonNetwork.LoadLevel("Multiplayer Game Board");
         //photonView.RPC("LoadScene", RpcTarget.All);
 
     }
@@ -84,15 +90,37 @@ public class PhotonManager : MonoBehaviourPunCallbacks
     public void ExitButton()
     {
         PhotonNetwork.LeaveRoom();
+        scrollView.SetActive(true);
+        roomContent.SetActive(false);
     }
 
     public override void OnLeftRoom()
     {
         Debug.Log("You left room");
     }
+    public void StartGame()
+    {
+        if(readyCheck)
+        {
+            readyCheck = false;
+            photonView.RPC("LoadScene", RpcTarget.All);
+        }
+        else
+        {
+            photonView.RPC("Ready", RpcTarget.Others);
+        }
+        Debug.Log ("You are ready");
+    }
     [PunRPC]
     public void LoadScene()
     {
         PhotonNetwork.LoadLevel("Multiplayer Game Board");
     }
+    [PunRPC]
+    public bool Ready()
+    {
+        readyCheck=true;
+        return readyCheck;
+    }
+
 }
--- Assets/Scripts/Game Board/Multiplayer/Room.cs
+++ Assets/Scripts/Game Board/Multiplayer/Room.cs
@@ -9,11 +9,9 @@ using TMPro;
 public class Room : MonoBehaviour
 {
     [SerializeField] TMP_Text textName;
-    [SerializeField] TMP_Text playerCount;
 
     public void SetInfo(RoomInfo info)
     {
         textName.text = info.Name;
-        playerCount.text = info.PlayerCount +"/" + info.MaxPlayers;
     }
 }
